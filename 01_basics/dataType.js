0993cb4edb847a403a46fbaa75f1a8f7ab60d58c
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-01-09 11:10:34 +0000

    dataType

--- 01_basics/dataType.js
+++ 01_basics/dataType.js
@@ -6,3 +6,38 @@
 console.log(3+
     3)  // this is correct but bad readability so focus on it also 
     
+    /**
+     * DateTypes
+     *  
+     * Primitive Data Type
+     * 
+     *   1. number
+     *         # 2 to power 53 is range 
+     *         # example let age = 25;
+     *   2. bigint
+     *         # use for large range of number
+     *         # example let age = 2578456896543988465312;
+     * 
+     *   3. String
+     *         # value inside double qoutes ""
+     *         # example let Name = "pragyesh";
+     *   4. boolean
+     *         # its is two value true/false
+     *         # example let isEmpty = true;
+     *   5. null
+     *         # standalone value something which is empty 
+     *         # it is not undefined but it is empty in hindi we call it khali 
+     *         # if Any api doesnt send any response then we it mean it is null 
+     *   6. undefined
+     *         # mean no value is assigned yet 
+     *         # example let isEmpty ;
+     *   7. symbol
+     *         # it is used when we create number of components and we have to point some of them to be unique then we use symbol
+     *         # use to mark uniqueness  ;
+     *   
+     *   Non - Primitive Data Type 
+     *  
+     *    1 Object 
+     *       # null is object type 
+     */
+    console.table([typeof(undefined),typeof(null)]) //  null --> object type , undefined --> undefined type
