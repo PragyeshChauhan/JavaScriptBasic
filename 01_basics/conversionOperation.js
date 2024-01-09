059057cce7e76fd6cde6d3231d56a3aefbc441d3
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-01-09 12:42:11 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,38 @@
+
+let score =true
+let valueInString =String(score)
+console.log(typeof score);
+console.log(typeof valueInString);
+console.log(valueInString);  // NaN (Not a Number) 
+/**
+ * NaN : its is also type of type.
+ * 
+ * conversion Notes 
+ * 
+ * for conversion from other variable to Number 
+ *  1. "33"          => 33 
+ *  2. "33abxc"      => NaN
+ *  3. null          => 0 
+ *  4. undefined     => NaN 
+ *  5. true          => 1 
+ * 
+ * for conversion to boolean 
+ *  1. 1            => true 
+ *  2. 0            => false
+ *  3. ""           => false 
+ *  4. "someThing"  => true 
+ *  5. null         => false
+ *  6. undefined     => false
+ * 
+ * for conversion to String 
+ *  1. null            => null 
+ *  
+ */
+
+/**let isloggedIn 
+ 
+let booleanConversionVariable = Boolean(isloggedIn)
+console.log(typeof isloggedIn)
+console.log(isloggedIn)
+console.log(typeof booleanConversionVariable)
+console .log(booleanConversionVariable)*/
