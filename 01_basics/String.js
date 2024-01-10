a3d87abd315e9dd08965d63eaf89476c72c237b2
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-01-10 14:12:42 +0000

    String , slice,splice

---
+++ 01_basics/String.js
@@ -0,0 +1,43 @@
+/**
+ * Theroy Point
+ *  1. can we  written inside '' single quotes  and " "  double quotes
+ *  2.  for concatination normal + sign is outdated now we use backticks for that   
+ */
+let name ='me'
+console.log(`this is backticks use by ${name} as it is better way  `)
+
+const gameName =new String ('Mario')
+console.log(gameName.__proto__)
+console.log(gameName.blink())
+
+const anotherString =gameName.slice(-3,-1)  //start from last 
+console.log(anotherString) // Ma
+
+/**
+ * ###################slice################3
+ * slice(start ,End) in this start point object is include and end point object will neglected 
+ * 
+ * slice(start)  in this case start point object will neglected
+ * 
+ * Negative 
+ * 
+ * slice(-start ,End)  in this case it start from back side or jaha pe rukega us object se then it move forward basically 
+ *  gameName.slice(-3,1) --> empty as -3 mean r and from r it goes to where as 1 is small so empty 
+ *  gamename.slice(-3,-1) --> ri -3 mean r and -1 mean o but o will not include 
+  * 
+ */
+
+// const array =["hi","hello","how are you "]
+// let spliceArray =array.splice(0,2,"hey") 
+// console.log(spliceArray)
+/**
+ * ####################Splice###############
+ * 
+ * here Splice(startpoint , kitne_hatane_hai , what_to_place)
+ * it will change the original Array and return the new remove array 
+ * here 
+ * array = [ 'hey', 'how are you ' ]
+ * spliceArray = [ 'hi', 'hello' ]
+ */
+// console.log(array)
+// 2:23
--- 01_basics/dataTypeSummery.js
+++ 01_basics/dataTypeSummery.js
@@ -31,3 +31,37 @@ let myFunction = function(){
 }
 console.log(typeof myFunction()) // Object
 console.log(typeof myFunction)  // function
+
+/**
+ * Memory
+ *   1. Stack 
+ *       Primitive value store in this Stack area
+ *       when we take then reference from stack we always get copy not the original reference and all the modification are done '
+ *       in the copied reference value
+ *   2. Heap 
+ *       non  Primitive value store in this area
+ *       when we take then from Heap we always get the original reference and all the modification are done in 
+ *      original reference value 
+ * 
+ * example : -
+ *  */ 
+
+let myEmail ="www.pragyesh.com@gmail.com"
+let secondEmail = myEmail
+secondEmail ="pragyeshchauhan01245@gmail.com"
+console.log(myEmail)
+console.log(secondEmail)
+
+let user = {
+    name : "Rishu Thakur",
+    age : 25
+}
+let anotherUser =user
+
+user.name=" Pragyesh thakur"
+
+console.log(user)
+console.log(anotherUser)
+
+// example by draw picture : https://excalidraw.com/#json=nzPpUMX7soG6gX4x54UqX,dAqoEGPrWLz1U1AKG6dfkg 
+
