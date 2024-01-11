69951ea758788a2d46b45852e231caf32e2bbd52
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-01-11 05:42:45 +0000

    String

--- 01_basics/String.js
+++ 01_basics/String.js
@@ -40,4 +40,12 @@ console.log(anotherString) // Ma
  * spliceArray = [ 'hi', 'hello' ]
  */
 // console.log(array)
-// 2:23
+const url ="hhtps://pragyesh.com/pragyesh%20chauhan"
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('.com'))
+
+// String to Array
+console.log(url.split('//'))
+
+// console.log(url.small())
