e8988cf20fe2e7bad2948d55f949d7095004c32a
PragyeshChauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2024-01-11 11:17:31 +0530

    Add files via upload

---
+++ 02_basics/arrays.js
@@ -0,0 +1,16 @@
+/**
+ * +++++++++++++++++Arrays++++++++++++++++++
+ * 
+ * #  It is collection of objects in single variable and also can collect heterogenous type of value 
+ * # arrays in js make sallows copyings when we make copy of array 
+ *      
+ *      > sallows copy : Is a copy in which properties share the references 
+ *      > deep copy : Is a copy in which properties do not share the references 
+ */
+const array =[0,2,2,3,"Singh"]
+const array2 = new Array(1,2,3,"singh")
+
+console.table([array,array2])
+
+// Array Methods 
+// 3:18
